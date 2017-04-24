325a658b1cceb84d71635d77a81ff4fdcfad97f6
Michael Stork <mstork@stream9.net>
2017-04-24 15:01:01 -0600

    Skills component indentation

--- src/partials/Skills.js
+++ src/partials/Skills.js
@@ -2,26 +2,26 @@ import React from 'react';
 import './Skills.css';
  
 const SkillsList = (props) => {
-	return (
-		<div className="Skills-list">
-			{props.skills.map((group, groupIndex) =>
-				<div className="Skills-list-group" key={groupIndex}>
-					{group.map((list, listIndex) =>
-						<div key={listIndex}>
-							<p>{list.title}</p>
-							<ul>
-								{list.skills.map((skill, skillIndex) =>
-									<li key={skillIndex}>
-										<a href={skill.url} target="_blank">{skill.title}</a>
-									</li>
-								)}
-							</ul>
-						</div>
-					)}
-				</div>
-			)}
-		</div>
-	)
+  return (
+    <div className="Skills-list">
+      {props.skills.map((group, groupIndex) =>
+        <div className="Skills-list-group" key={groupIndex}>
+          {group.map((list, listIndex) =>
+            <div key={listIndex}>
+              <p>{list.title}</p>
+              <ul>
+                {list.skills.map((skill, skillIndex) =>
+                  <li key={skillIndex}>
+                    <a href={skill.url} target="_blank">{skill.title}</a>
+                  </li>
+                )}
+              </ul>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default SkillsList;
